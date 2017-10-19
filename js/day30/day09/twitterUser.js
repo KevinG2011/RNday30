0dc9f550227665c2072de25d4e953fa161f49b7e
lijia <lijia@huajiao.tv>
2017-10-19 21:24:05 +0800

    add image panel.

--- js/day30/day09/twitterUser.js
+++ js/day30/day09/twitterUser.js
@@ -2,16 +2,22 @@
 
 import React, { Component } from 'react';
 import {
-	View
+	View,
+	Image
 } from 'react-native';
+import { Util } from '../../component/common/';
 
 class TwitterUser extends Component {
-  props: Props;
-
 	render() {
 		const { data } = this.props;
 		return (
 			<View style={styles.container}>
+				<View style={styles.userPanel}>
+					<Image
+					  style={styles.panelImg}
+					  source={require('./img/banner.png')}
+					/>
+				</View>
 			</View>
 		);
 	}
@@ -24,6 +30,14 @@ const styles = {
 		flex: 1,
 		backgroundColor: '#666',
 	},
+	userPanel: {
+		flex: 1,
+		backgroundColor: 'white',
+		height: 300,
+	},
+	panelImg: {
+		width: Util.size.width,
+		height: 150,
+	},
 };
-
 
