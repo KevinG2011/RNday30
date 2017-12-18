9ac84018e5431befd2763c30210a3da393bf8288
lijia <lijia@huajiao.tv>
2017-12-18 11:15:15 +0800

    prepare for day14.

---
+++ js/day30/day14/index.js
@@ -0,0 +1,29 @@
+'use strict';
+
+import React, { Component } from 'react';
+import {
+	View
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+
+class TinderApp extends Component {
+	render() {
+		return (
+			<View style={styles.container}>
+				<View style={styles.nav}>
+
+				</View>
+			</View>
+		);
+	}
+}
+
+export default TinderApp;
+
+const styles = {
+	container: {
+		flex: 1,
+	},
+};
+
